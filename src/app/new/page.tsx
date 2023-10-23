c8217010fc7cc7a561e4dda73d1e8cd80e88fca1
rubensilva3 <ruben.silva@emma-sleep.com>
2023-10-23 17:46:10 +0100

    first commit

---
+++ src/app/new/page.tsx
@@ -0,0 +1,44 @@
+import { prisma } from "@/db";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+  "use server";
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid title");
+  }
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/");
+}
+
+export default function Page() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl"> New </h1>
+      </header>
+      <form action={createTodo} className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+          >
+            Back
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
